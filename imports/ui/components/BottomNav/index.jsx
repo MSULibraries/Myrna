7694291cd4abde1c9c70dd2c11eb59c727018186
Brooks Becton <broabect@ut.utm.edu>
2017-11-07 09:59:13 -0600

    Bottom nav only shows up if user is logged in

--- imports/ui/components/BottomNav/index.jsx
+++ imports/ui/components/BottomNav/index.jsx
@@ -2,6 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { userLoggedIn } from './../../../../lib/user';
+
+const BottomNav = () =>
+  (Meteor.user() ? (
+    <BottomNavContainer>
+      <Item>
+        <BottomNavLink to="cart">
+          Cart
+          <img src="icons/ic_shopping_cart_white_24px.svg" alt="Cart Icon" />
+        </BottomNavLink>
+      </Item>
+      <Item>
+        <BottomNavLink to="orders">
+          Orders
+          <img src="icons/ic_list_white_24px.svg" alt="Orders Icon" />
+        </BottomNavLink>
+      </Item>
+      <Item>
+        <BottomNavLink to="profile">
+          Profile
+          <img src="icons/ic_account_box_white_24px.svg" alt="Profile Icon" />
+        </BottomNavLink>
+      </Item>
+    </BottomNavContainer>
+  ) : (
+    <div />
+  ));
+
 const height = '64px';
 
 const BottomNavContainer = styled.nav`
@@ -25,28 +53,5 @@ const BottomNavLink = styled(Link)`
   text-align: center;
   text-decoration: none;
 `;
-
-const BottomNav = () => (
-  <BottomNavContainer>
-    <Item>
-      <BottomNavLink to="cart">
-        Cart
-        <img src="icons/ic_shopping_cart_white_24px.svg" alt="Cart Icon" />
-      </BottomNavLink>
-    </Item>
-    <Item>
-      <BottomNavLink to="orders">
-        Orders
-        <img src="icons/ic_list_white_24px.svg" alt="Orders Icon" />
-      </BottomNavLink>
-    </Item>
-    <Item>
-      <BottomNavLink to="profile">
-        Profile
-        <img src="icons/ic_account_box_white_24px.svg" alt="Profile Icon" />
-      </BottomNavLink>
-    </Item>
-  </BottomNavContainer>
-);
 
 export default BottomNav;
