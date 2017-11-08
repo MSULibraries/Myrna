a4ec148376e8ee29992a2b0c8350fccbf3e13f5d
Brooks Becton <broabect@ut.utm.edu>
2017-11-08 15:19:55 -0600

    Added some padding to bottom of pages for bottom nav

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -23,22 +23,25 @@ const Routes = () => (
   <Router>
     <div>
       <MainNav />
-      <Switch>
-        <AuthRoute exact path="/addresses/" component={AddressesPage} />
-        <AuthRoute exact path="/addresses/:option" component={AddressesPage} />
-        <AuthRoute path="/cart" component={CartPage} />
-        <AuthRoute exact path="/orders" component={OrdersPage} />
-        <AuthRoute exact path="/payment/success" component={PaymentSuccess} />
-        <AuthRoute exact path="/profile" component={ProfilePage} />
+      {/* Making room for content covered by bottom nav */}
+      <div style={{ marginBottom: '84px' }}>
+        <Switch>
+          <AuthRoute exact path="/addresses/" component={AddressesPage} />
+          <AuthRoute exact path="/addresses/:option" component={AddressesPage} />
+          <AuthRoute path="/cart" component={CartPage} />
+          <AuthRoute exact path="/orders" component={OrdersPage} />
+          <AuthRoute exact path="/payment/success" component={PaymentSuccess} />
+          <AuthRoute exact path="/profile" component={ProfilePage} />
 
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/policies" component={PoliciesPage} />
-        <Route exact path="/products" component={ProductsContainer} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/policies" component={PoliciesPage} />
+          <Route exact path="/products" component={ProductsContainer} />
 
-        <Route component={NotFoundPage} />
-      </Switch>
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
       <BottomNav />
     </div>
   </Router>
