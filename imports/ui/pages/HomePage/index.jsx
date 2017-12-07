d68d8f75d1f0d22584b93e477622562b82c04178
Brooks Becton <broabect@ut.utm.edu>
2017-12-07 16:34:57 -0600

    Created <HeroSection/>

--- imports/ui/pages/HomePage/index.jsx
+++ imports/ui/pages/HomePage/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-grid-system';
+import { Container } from 'react-grid-system';
 import Helmet from 'react-helmet';
-import styled from 'styled-components';
 
-import { media } from './../../breakpoints';
+import HeroSection from './HeroSection';
 
 export const HomePage = () => (
   <div>
@@ -19,54 +18,9 @@ export const HomePage = () => (
     <Container>
       <h1>Welcome</h1>
     </Container>
-    <HeroContainer>
-      <HeroImageContainer>
-        <HeroImage src="http://via.placeholder.com/750x300" alt="" />
 
-        <HeroRightImage src="http://via.placeholder.com/400x300" alt="" />
-      </HeroImageContainer>
-    </HeroContainer>
+    <HeroSection />
   </div>
 );
-
-const HeroImageContainer = styled.div`
-  ${media.desktop`
-  `};
-
-  background-color: #bca6ca;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-
-  padding: 1%;
-`;
-
-const HeroContainer = styled.div`
-  background-color: #cbbbd6;
-  display: flex;
-  justify-content: center;
-`;
-
-const MobileNavContainer = styled.div`
-  display: none;
-`;
-
-const HeroImage = styled.img`
-  ${media.giant`
-    margin-right: auto;
-  `};
-  height: 100%;
-  margin-right: 2vw;
-  width: 100%;
-`;
-
-const HeroRightImage = styled.img`
-  ${media.giant`
-    display: none;
-  `};
-  display: flex;
-  height: 100%;
-  width: 100%;
-`;
 
 export default HomePage;
