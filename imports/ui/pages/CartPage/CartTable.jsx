50dcf4d22b7ffc1623af4c7aab2dc9785c888c9b
Brooks Becton <broabect@ut.utm.edu>
2018-01-25 11:24:23 -0600

    Added item image to cart

--- imports/ui/pages/CartPage/CartTable.jsx
+++ imports/ui/pages/CartPage/CartTable.jsx
@@ -14,6 +14,7 @@ import styled from 'styled-components';
 import CartIcon from './components/CartIcon';
 import { media } from './../../breakpoints';
 import Cart from './../../../api/cart';
+import ItemDesc from './../../../api/ItemDesc';
 
 // Adjusted contrast to help with a11y
 const darkerTableHeaders = {
@@ -27,14 +28,60 @@ const alignCenter = {
 const centerColumn = {
   display: 'flex',
   alignItems: 'center',
+  justifyContent: 'center',
 };
 
 class CartTable extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      itemDesc: {},
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.itemDesc !== this.props.itemDesc) {
+      this.updateItemDescState(this.props.itemDesc);
+    }
+  }
+
   /**
    * Removes product from user's cart collection
    */
   removeProductFromCart = id => {
     Meteor.call('cart.remove', id);
+  };
+
+  /**
+   * Updates the component state of itemDesc
+   * takes in an array of products and sets state
+   * to an object by productId
+   * @param {Array} - newItemDesc
+   */
+  updateItemDescState = newItemDesc => {
+    const itemDescById = {};
+    newItemDesc.forEach(item => {
+      itemDescById[item._id] = item;
+    });
+    this.setState({ itemDesc: itemDescById });
+  };
+
+  renderItemIcon = id => {
+    const item = this.state.itemDesc[id];
+    if (item) {
+      return (
+        <CartIcon
+          productId={id}
+          src={`images/clothing/${item.category.toLowerCase()}/${item.oldId}/small/${
+            JSON.parse(item.description).picture_1
+          }`}
+          alt={`images/clothing/${item.category}/${item.oldId}/small/${
+            JSON.parse(item.description).picture_1
+          }`}
+        />
+      );
+    }
   };
 
   render() {
@@ -43,7 +90,7 @@ class CartTable extends Component {
         <Table>
           <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
             <TableRow>
-              <TableHeaderColumn style={{ ...darkerTableHeaders, width: '50px' }} />
+              <TableHeaderColumn style={{ ...darkerTableHeaders, height: '80px', width: '80px' }} />
               <MobileHiddenColumn style={darkerTableHeaders}>Id</MobileHiddenColumn>
               <TableHeaderColumn style={darkerTableHeaders}>Status</TableHeaderColumn>
               {/* <TableHeaderColumn style={darkerTableHeaders}>Added On </TableHeaderColumn> */}
@@ -56,8 +103,8 @@ class CartTable extends Component {
           <TableBody displayRowCheckbox={false}>
             {this.props.cartItems.map(item => (
               <TableRow key={item._id}>
-                <TableRowColumn style={{ width: '50px' }}>
-                  <CartIcon src="http://via.placeholder.com/50x50" />
+                <TableRowColumn style={{ height: '80px', width: '80px', padding: '24px' }}>
+                  {this.renderItemIcon(item.productId)}
                 </TableRowColumn>
                 <MobileHiddenColumn>{item._id}</MobileHiddenColumn>
                 <TableRowColumn>
@@ -91,10 +138,15 @@ const MobileHiddenColumn = styled(TableRowColumn)`
   ${media.desktop`display: none !important;`} display: table-cell !important;
 `;
 
-export default withTracker(props => {
+export default withTracker(({ cartItems }) => {
   Meteor.subscribe('cart');
+  Meteor.subscribe('itemDesc');
+  const cartProductIds = cartItems.map(({ productId }) => productId);
 
   return {
     cartItems: Cart.find({}).fetch(),
+    itemDesc: ItemDesc.find({
+      _id: { $in: cartProductIds },
+    }).fetch(),
   };
 })(CartTable);
